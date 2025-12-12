25bcc0fcf3bb0107dae79fe7e7c77e8a4f24bc8e
Melisha <melisha.evans06@gmail.com>
2025-12-12 08:45:05 -0700

    updated

--- src/app/services/stargate.service.ts
+++ src/app/services/stargate.service.ts
@@ -6,10 +6,8 @@ import { AstronautDutyResponse } from '../models/astronaut-duty.model';
 
 @Injectable({ providedIn: 'root' })
 export class StargateService {
-  // TODO: change to your Swagger base URL (copy it exactly)
-private readonly baseUrl = 'http://localhost:7204';
 
-
+    private readonly baseUrl = 'http://localhost:7204';
 
   constructor(private http: HttpClient) {}
 
